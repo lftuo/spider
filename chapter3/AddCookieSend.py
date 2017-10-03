2c85f3ce58e7b155958548daed538d8dc3bf8e71
lftuo <909709223@qq.com>
2017-10-03 14:18:10 +0800

    delete

--- chapter3/AddCookieSend.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding:utf8 -*-
-# 自定义cookie的值并发送
-import requests
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; MAC OS)'
-headers = {'User-Agent':user_agent}
-cookies = dict(name='lftuo',age='18')
-r = requests.get('http://www.baidu.com',headers = headers,cookies = cookies)
-print r.text
