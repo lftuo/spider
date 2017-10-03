3551996b57085288b32f6139cae10146f3a822a3
lftuo <909709223@qq.com>
2017-10-03 14:19:10 +0800

    Delete HeadersGet.py

--- chapter3/HeadersGet.py
+++
@@ -1,5 +0,0 @@
-import requests
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; MAC OS)'
-headers = {'User-Agent':user_agent}
-r = requests.get('http://www.baidu.com',headers=headers)
-print r.content
