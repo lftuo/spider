366f574142d2dfb98d8bd8f35d846761a97be493
tuotuo <909709223@qq.com>
2017-10-04 20:32:00 +0800

    fina_all用法

--- com/spider/htmlparse/FindAllTest.py
+++ com/spider/htmlparse/FindAllTest.py
@@ -13,8 +13,8 @@ html_str = """
 <p class="title"><b>The Dormouse's story</b></p>
 <p class="story"><b>Once upon a time there were three little sisters; and their names were 
 <a href="http://www.github.com/elsie" class="sister" id="link1"><!-- Elsie --></a>
-<a href="http://www.github.com/lacie" class="sister" id="link2"><!-- Elsie --></a> and 
-<a href="http://www.github.com/tillie" class="sister" id="link3"><!-- Elsie -->Tillie</a>;
+<a href="http://www.github.com/lacie" class="sister" id="link2"><!-- Lacie --></a> and 
+<a href="http://www.github.com/tillie" class="sister" id="link3"><!-- Tillie -->Tillie</a>;
 and they lived at the bottle of a well.</p>
 <p class="story">...</p>
 """
@@ -39,3 +39,26 @@ def hasClass_Id(tag):
     return tag.has_attr('class') and tag.has_attr('id')
 print soup.find_all(hasClass_Id)
 print '\n'
+# kwargs参数：字符串／正则表达式／列表／True
+print '--------------- kwargs --------------'
+print soup.find_all(id="link2")
+import re
+print soup.find_all(href=re.compile("tillie"))
+# 无论ID是何值，存在则输出
+print soup.find_all(id=True)
+# class是python关键字，需在前加'_'
+print soup.find_all("a",class_="sister")
+# 使用多个名字的参数，可以同时过滤tag的多个属性
+print soup.find_all(href=re.compile("elsie"),id="link1")
+# text：搜索文档中字符串内容：数值可以是字符串／正则表达式／列表／True／方法等
+print '--------------- text ----------------'
+print soup.find_all(text="Elsie")
+print soup.find_all(text=["Tillie","Elsie","Lacie"])
+print soup.find_all(text=re.compile("Dormouse"))
+print soup.find_all("a",text="Elsie")
+#limit：限制返回结果条数
+print '-------------- limit ----------------'
+print soup.find_all("a",limit=2)
+#recursive：搜索tag的直接子节点：recursive=False
+print soup.find_all("title")
+print soup.find_all("title",recursive=False)
