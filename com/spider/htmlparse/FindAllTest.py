bfb8ee32efb93b9955c8b1af2556944e6d56e6c8
tuotuo <909709223@qq.com>
2017-10-04 09:12:11 +0800

    .string strings stripped_strings用法

--- com/spider/htmlparse/FindAllTest.py
+++ com/spider/htmlparse/FindAllTest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf8 -*-
 # @Author : MrTuo
-# @Time : 2017/10/3 下午12:05
-# @File : __init__.py.py
+# @Time : 2017/10/4 上午9:11
+# @File : FindAllTest.py
 # @Software : PyCharm
