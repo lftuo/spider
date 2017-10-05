c6acf78993797cce1782764af99b0a5178a2290f
tuotuo <909709223@qq.com>
2017-10-05 17:41:32 +0800

    发送html字符串到email

---
+++ com/spider/spidermemory/core/SendEmailTest.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+# @Author : MrTuo
+# @Time : 2017/10/5 下午5:20
+# @File : SendEmailTest.py
+# @Software : PyCharm
+# 发送html字符串到email
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr,formataddr
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name,'utf-8').encode(),addr))
+# 发件人地址
+from_addr = '***********@163.com'
+# 邮箱密码
+password = '**********'
+# 收件人地址
+to_addr = '*********@qq.com'
+# 163网易邮箱服务器地址
+smtp_server = 'smtp.163.com'
+# 设置邮件信息
+msg = MIMEText('Python爬虫运行异常，异常信息为HTTP 403','plain','utf-8')
+msg['From'] = _format_addr('一号爬虫 <%s>' % from_addr)
+msg['To'] = _format_addr('管理员 <%s>' % to_addr)
+msg['Subject'] = Header('一号爬虫运行状态','utf-8').encode()
+
+# 设置邮箱信息
+server = smtplib.SMTP(smtp_server,25)
+server.login(from_addr,password)
+server.sendmail(from_addr,[to_addr],msg.as_string())
+server.quit()
